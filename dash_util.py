4b565aee90a4a84628c68c5913c2084668ea8da6
zs856 <42588942+zs856@users.noreply.github.com>
2021-03-08 02:06:33 +0800

    【ADD】加入了可视乎分析方法，但是显示效果还可以进一步被改进，如加入信息过滤功能防止数据点过于密集

--- dash_util.py
+++ dash_util.py
@@ -1,6 +1,8 @@
 import dash
 import dash_html_components as html
 import pandas as pd
+import plotly.express as px
+import dash_core_components as dcc
 def generate_table(dataframe, max_rows=10):
     return html.Table([
         html.Thead(
@@ -12,14 +14,22 @@ def generate_table(dataframe, max_rows=10):
             ]) for i in range(min(len(dataframe), max_rows))
         ])
     ])
-
+def draw_graph(df):
+    fig = px.scatter(df, x="项目名称", y="成交参考价格 \n（元/平方米）",
+                     size="成交参考价格 \n（元/平方米）", color="行政区", hover_name="项目名称",
+                     log_y=True)
+    return fig
 def run_server(df):
     external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
     app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+    fig = draw_graph(df)
     app.layout = html.Div(children=[
         html.H4(children='深圳二手房参考价(2021)'),
-        generate_table(df,9999999999)
+        generate_table(df,9999999999),
+        dcc.Graph(
+            id='可视化分析',
+            figure=fig
+        )
     ])
     app.run_server(debug=True)
